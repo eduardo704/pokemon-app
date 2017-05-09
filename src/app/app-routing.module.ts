347fe570cfc7e458701a3b733653004ac6fc5079
Eduardo Vargas <eduardo704@gmail.com>
2017-05-09 15:19:34 +0200

    removed unused component from routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {MyNewComponentComponent} from './my-new-component/my-new-component.component';
 import {PokemonListComponent} from './pokemons/pokemon-list/pokemon-list.component';
 import {PageNotFoundComponent} from './not-found.component';
 
 const routes : Routes = [
-  {
-    path: 'banana',
-    component: MyNewComponentComponent
-  },
   { path: '',  redirectTo: '/list',  pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
